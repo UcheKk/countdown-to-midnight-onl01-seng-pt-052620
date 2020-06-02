aee2f132c539e4a4cd5e664c8093f5063cb9ea58
Uche Nweke Jr <unwekejr@gmail.com>
2020-06-02 22:19:20 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,7 +10,12 @@ end
   return "HAPPY NEW YEAR!"
 end
 
-  def countdown_with_sleep(sleep)
+  def countdown_with_sleep(n)
     
     while n > 0
-    
+    sleep 1
+    puts "#{n} SECOND(S)!"
+    n -= 1 
+  end
+  
+  return "HAPPY NEW YEAR!"
